1b2a8fef4958ce856a698580b331641a8a227bb6
ohjaesik <96814750+ohjaesik@users.noreply.github.com>
2026-07-09 13:43:40 +0900

    refactor: run bootstrap graph through expert executor

--- app/company_bootstrap/workflow.py
+++ app/company_bootstrap/workflow.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.graph import END, START, StateGraph
 
-from app.agents.runtime import with_agent_contract
+from app.agents.expert_executor import expert_executed_node
 from app.company_bootstrap.nodes import (
     company_profile_agent_node,
     process_discovery_agent_node,
@@ -17,9 +17,9 @@ from app.company_bootstrap.state import BootstrapState
 def build_bootstrap_supervisor_graph():
     builder = StateGraph(BootstrapState)
 
-    builder.add_node("company_profile_agent", with_agent_contract("company_profile_agent", company_profile_agent_node))
-    builder.add_node("source_ingestion_agent", with_agent_contract("source_ingestion_agent", source_ingestion_agent_node))
-    builder.add_node("process_discovery_agent", with_agent_contract("process_discovery_agent", process_discovery_agent_node))
+    builder.add_node("company_profile_agent", expert_executed_node("company_profile_agent", company_profile_agent_node))
+    builder.add_node("source_ingestion_agent", expert_executed_node("source_ingestion_agent", source_ingestion_agent_node))
+    builder.add_node("process_discovery_agent", expert_executed_node("process_discovery_agent", process_discovery_agent_node))
 
     builder.add_edge(START, "company_profile_agent")
     builder.add_edge("company_profile_agent", "source_ingestion_agent")
